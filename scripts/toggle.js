8ec24feda134da715cbf474dd62eb00472c3f3b3
Hadley Slucher <hadleyslucher@gmail.com>
2021-02-07 20:17:47 -0500

    Added about page, project page, created carousel

---
+++ scripts/toggle.js
@@ -0,0 +1,55 @@
+/**
+ * 
+ * SERVICES TOGGLE
+ *  
+ */
+
+const socialMedia = document.querySelector('.socialMedia');
+const webDesign = document.querySelector('.webDesign');
+const branding = document.querySelector('.branding');
+const photography = document.querySelector('.photography');
+
+const socialMedia__text = document.querySelector('.socialMedia__text');
+const webDesign__text = document.querySelector('.webDesign__text');
+const branding__text = document.querySelector('.branding__text');
+const photography__text = document.querySelector('.photography__text');
+
+const socialMediaIcon = document.querySelector('button.socialMedia svg');
+const webDesignIcon = document.querySelector('button.webDesign svg');
+const brandingIcon = document.querySelector('button.branding svg');
+const photographyIcon = document.querySelector('button.photography svg');
+
+
+webDesign.addEventListener('click', () => {
+    webDesign__text.style.display = 'block';
+    webDesign.style.background = 'var(--second)';
+    webDesignIcon.style.fill = 'var(--font)';
+    socialMedia__text.style.display = 'none';
+    branding__text.style.display = 'none';
+    photography__text.style.display = 'none';
+});
+
+socialMedia.addEventListener('click', () => {
+    socialMedia__text.style.display = 'block';
+    socialMedia.style.background = 'var(--second)';
+    socialMediaIcon.style.fill = 'var(--font)';
+    webDesign__text.style.display = 'none';
+    branding__text.style.display = 'none';
+    photography__text.style.display = 'none';
+});
+
+branding.addEventListener('click', () => {
+    branding__text.style.display = 'block';
+    branding.style.background = 'var(--second)';
+    brandingIcon.style.fill = 'var(--font)';
+    webDesign__text.style.display = 'none';
+    socialMedia__text.style.display = 'none';
+    photography__text.style.display = 'none';
+});
+
+photography.addEventListener('click', () => {
+    photography__text.style.display = 'block';
+    socialMedia__text.style.display = 'none';
+    webDesign__text.style.display = 'none';
+    branding__text.style.display = 'none';
+});
